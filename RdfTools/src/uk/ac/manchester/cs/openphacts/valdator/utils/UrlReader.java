c12bc442f905a5bdfa7dcc19f8edb29b911175d0
Christian-B <brenninc@cs.man.ac.uk>
2013-04-30 15:16:37 +0100

    Added/ corrected copyright

--- RdfTools/src/uk/ac/manchester/cs/openphacts/valdator/utils/UrlReader.java
+++ RdfTools/src/uk/ac/manchester/cs/openphacts/valdator/utils/UrlReader.java
@@ -1,3 +1,22 @@
+// OpenPHACTS RDF Validator,
+// A tool for validating and storing RDF.
+//
+// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
+// Copyright 2012-2013  University of Manchester
+// Copyright 2012-2013  OpenPhacts
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 package uk.ac.manchester.cs.openphacts.valdator.utils;
 
 import java.io.BufferedReader;
@@ -7,25 +26,18 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
-import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.UnknownHostException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.zip.GZIPInputStream;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPConnectionClosedException;
 import org.apache.commons.net.ftp.FTPReply;
 import uk.ac.manchester.cs.openphacts.valdator.rdftools.FtpListener;
-import uk.ac.manchester.cs.openphacts.valdator.rdftools.FtpListener;
 import uk.ac.manchester.cs.openphacts.valdator.rdftools.VoidValidatorException;
-import uk.ac.manchester.cs.openphacts.valdator.rdftools.VoidValidatorException;
-import uk.ac.manchester.cs.openphacts.valdator.utils.AccountInfo;
 
 
 /**

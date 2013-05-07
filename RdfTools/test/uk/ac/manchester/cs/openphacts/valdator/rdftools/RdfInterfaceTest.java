dd136e775c5d6f2ea68a47b7ac31ff0288a2fbb9
Christian-B <brenninc@cs.man.ac.uk>
2013-05-07 08:14:25 +0100

    updated copyright

--- RdfTools/test/uk/ac/manchester/cs/openphacts/valdator/rdftools/RdfInterfaceTest.java
+++ RdfTools/test/uk/ac/manchester/cs/openphacts/valdator/rdftools/RdfInterfaceTest.java
@@ -1,7 +1,22 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// OpenPHACTS RDF Validator,
+// A tool for validating and storing RDF.
+//
+// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
+// Copyright 2012-2013  University of Manchester
+// Copyright 2012-2013  OpenPhacts
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 package uk.ac.manchester.cs.openphacts.valdator.rdftools;
 
 import java.io.ByteArrayOutputStream;
@@ -14,11 +29,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openrdf.model.Resource;
-import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
 import org.openrdf.model.Value;
 import org.openrdf.model.impl.URIImpl;
-import org.openrdf.query.TupleQueryResultHandler;
 import org.openrdf.query.resultio.sparqlxml.SPARQLResultsXMLWriter;
 import org.openrdf.rio.RDFFormat;
 
